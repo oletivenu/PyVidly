99a082fff505fb7b002dcb2eeb19354a5fadcbbe
Naga Venu Babu  Oleti <N.Oleti@midisglobal.local>
2020-01-15 19:58:47 +0400

    first commit

---
+++ movies/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='movies_index'),
+    path('<int:movie_id>', views.detail, name='movies_detail')
+]
